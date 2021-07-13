0e453bc726361ea56ad86c60e6532bf8e7b19c0b
Alex Rosa <alexr@gec.inatel.br>
2021-07-13 20:33:51 -0300

    tentativa de atualizar hora com a thread - Failed

--- app/src/main/java/com/alex/app02_relogio/MainActivity.java
+++ app/src/main/java/com/alex/app02_relogio/MainActivity.java
@@ -9,14 +9,20 @@ import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    private  ViewHolder mViewHolder = new ViewHolder();
+    private ViewHolder mViewHolder = new ViewHolder();
     private Runnable mRunnable;
-    private Handler mHandler = new Handler();
+    // Lida com a thread
+    //private Handler mHandler = new Handler(Looper.getMainLooper());
+
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -38,17 +44,45 @@ public class MainActivity extends AppCompatActivity {
         this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
         this.startClock();
+
     }
 
-    private void startClock(){
-        this.mRunnable = new Runnable() { // Runnable é uma interface
+    /**
+     * Inicializa o relógio de cabeceira
+     */
+    private void startClock() {
+
+        // Inicializa o calendário
+        final Calendar calendar = Calendar.getInstance();
+
+        // Lida com a thread
+        final Handler mHandler = new Handler(Looper.getMainLooper());
+
+        // Inicializa thread
+        this.mRunnable = new Runnable() {
             @Override
             public void run() {
+
+                // Obtém o tempo do celular e atribui ao calendário criado
+                calendar.setTimeInMillis(System.currentTimeMillis());
+
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minutes = calendar.get(Calendar.MINUTE);
+                int seconds = calendar.get(Calendar.SECOND);
+
+                // Converte e atribui
+                mViewHolder.textHourMinute.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minutes));
+                mViewHolder.textSeconds.setText(String.format(Locale.getDefault(), "%02d", seconds));
+
+
+                // Cálculo de quando a thread vai rodar novamente
                 long now = SystemClock.elapsedRealtime();
-                long next = 1000 + (1000 - (now % 1000));
+                long next = now + (1000 - (now % 1000));
                 mHandler.postAtTime(mRunnable, next);
             }
         };
+
+        // Faz com que a thread rode pela primeira vez
         this.mRunnable.run();
     }
 
